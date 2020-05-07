d7e5336ac34fabe0bab5a3897c85f3a90c96870e
Jordan Hewitt <hewitj2@gmail.com>
2020-05-07 02:09:10 -0700

    removed saved pictures. added db config defaults to jawsdb url.

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+var Url = require('url-parse')
+var JAWSURL = new Url(Env.get('JAWSDB_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -50,11 +53,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', JAWSURL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
+      user: Env.get('DB_USER', JAWSURL.username),
+      password: Env.get('DB_PASSWORD', JAWSURL.password),
+      database: Env.get('DB_DATABASE', JAWSURL.pathname.substr(1)),
       charset: Env.get('DB_CHARSET', 'utf8')
     }
   },
